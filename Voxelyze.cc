c889a5e4c2d48db00270aa399a00a69e3e3f2ec3
Filip Saulean <fsaulean@uvm.edu>
2020-01-30 19:18:55 -0500

    Add material and voxel objects

--- Voxelyze.cc
+++ Voxelyze.cc
@@ -33,6 +33,33 @@ Napi::Object Voxelyze::Init(Napi::Env env, Napi::Object exports) {
   return exports;
 }
 
+void setForce(const Napi::CallbackInfo& info) {
+  CVX_Voxel* voxel = reinterpret_cast<CVX_Voxel*>(info.Data());
+
+  float x = info[0].As<Napi::Number>().FloatValue();
+  float y = info[1].As<Napi::Number>().FloatValue();
+  float z = info[2].As<Napi::Number>().FloatValue();
+
+  voxel->external()->setForce(x, y, z);
+}
+
+void setFixedAll(const Napi::CallbackInfo& info) {
+  CVX_Voxel* voxel = reinterpret_cast<CVX_Voxel*>(info.Data());
+
+  voxel->external()->setFixedAll();
+}
+
+Napi::Value position(const Napi::CallbackInfo& info) {
+  CVX_Voxel* voxel = reinterpret_cast<CVX_Voxel*>(info.Data());
+  auto position = voxel->position();
+  Napi::Array positionArray = Napi::Array::New(info.Env(), 3);
+  positionArray[(uint32_t) 0] = Napi::Number::New(info.Env(), position.x);
+  positionArray[(uint32_t) 1] = Napi::Number::New(info.Env(), position.y);
+  positionArray[(uint32_t) 2] = Napi::Number::New(info.Env(), position.z);
+
+  return positionArray;
+}
+
 Voxelyze::Voxelyze(const Napi::CallbackInfo& info) : Napi::ObjectWrap<Voxelyze>(info) {
   Napi::Env env = info.Env();
   Napi::HandleScope scope(env);
@@ -46,18 +73,60 @@ Napi::Value Voxelyze::materialCount(const Napi::CallbackInfo& info) {
   return Napi::Number::New(info.Env(), materialCount);
 }
 
+void setColor(const Napi::CallbackInfo& info) {
+  CVX_Material* mat = reinterpret_cast<CVX_Material*>(info.Data());
+
+  if (info.Length() >= 3) {
+    float red = info[0].As<Napi::Number>().Int64Value();
+    float blue = info[1].As<Napi::Number>().Int64Value();
+    float green = info[2].As<Napi::Number>().Int64Value();
+    if (info.Length() == 3) {
+        mat->setColor(red, blue, green);
+    } else {
+        float alpha = info[3].As<Napi::Number>().Int64Value();
+        mat->setColor(red, blue, green, alpha);
+    }
+  } else {
+    Napi::TypeError::New(info.Env(), "Wrong number of arguments")
+            .ThrowAsJavaScriptException();
+  }
+}
+
+Napi::Value getColor(const Napi::CallbackInfo& info) {
+  CVX_Material* mat = reinterpret_cast<CVX_Material*>(info.Data());
+
+  Napi::Array colorArray = Napi::Array::New(info.Env(), 4);
+  colorArray[(uint32_t) 0] = Napi::Number::New(info.Env(), mat->red());
+  colorArray[(uint32_t) 1] = Napi::Number::New(info.Env(), mat->blue());
+  colorArray[(uint32_t) 2] = Napi::Number::New(info.Env(), mat->green());
+  colorArray[(uint32_t) 3] = Napi::Number::New(info.Env(), mat->alpha());
+
+  return colorArray;
+}
+
 Napi::Value Voxelyze::addMaterial(const Napi::CallbackInfo& info) {
+  Napi::Env env = info.Env();
   //std::string density = info[0].ToString();
   float youngsModulus = info[0].As<Napi::Number>().FloatValue();
   float density = info[1].As<Napi::Number>().FloatValue();
 
   int index = voxelyze->materialCount();
-  voxelyze->addMaterial(youngsModulus, density);
+  auto mat = voxelyze->addMaterial(youngsModulus, density);
 
-  return Napi::Number::New(info.Env(), index);
+  Napi::Object matObject = Napi::Object::New(env);
+  matObject.Set("index", index);
+  matObject.DefineProperties({
+    Napi::PropertyDescriptor::Function(env, matObject, "setColor", setColor, napi_property_attributes::napi_default, reinterpret_cast<void*>(mat)),
+    Napi::PropertyDescriptor::Function(env, matObject, "getColor", getColor, napi_property_attributes::napi_default, reinterpret_cast<void*>(mat)),
+  });
+
+
+
+  return matObject;
 }
 
 Napi::Value Voxelyze::setVoxel(const Napi::CallbackInfo& info) {
+  Napi::Env env = info.Env();
   int index = info[0].As<Napi::Number>().Int32Value();
   float x = info[1].As<Napi::Number>().FloatValue();
   float y = info[2].As<Napi::Number>().FloatValue();
@@ -66,7 +135,15 @@ Napi::Value Voxelyze::setVoxel(const Napi::CallbackInfo& info) {
   auto mat = voxelyze->material(index);
   auto voxel = voxelyze->setVoxel(mat, x, y, z);
 
-  return Napi::Number::New(info.Env(), index);
+  Napi::Object voxelObject = Napi::Object::New(env);
+
+  voxelObject.DefineProperties({
+    Napi::PropertyDescriptor::Function(env, voxelObject, "setForce", setForce, napi_property_attributes::napi_default, reinterpret_cast<void*>(voxel)),
+    Napi::PropertyDescriptor::Function(env, voxelObject, "position", position, napi_property_attributes::napi_default, reinterpret_cast<void*>(voxel)),
+    Napi::PropertyDescriptor::Function(env, voxelObject, "setFixedAll", setFixedAll, napi_property_attributes::napi_default, reinterpret_cast<void*>(voxel))
+  });
+
+  return voxelObject;
 }
 
 void Voxelyze::doLinearSolve(const Napi::CallbackInfo& info) {
@@ -128,26 +205,6 @@ Napi::Value Voxelyze::getGravity(const Napi::CallbackInfo& info) {
   return Napi::Number::New(info.Env(), voxelyze->gravity());
 }
 
-void setForce(const Napi::CallbackInfo& info) {
-  CVX_Voxel* voxel = reinterpret_cast<CVX_Voxel*>(info.Data());
-
-  float x = info[0].As<Napi::Number>().FloatValue();
-  float y = info[1].As<Napi::Number>().FloatValue();
-  float z = info[2].As<Napi::Number>().FloatValue();
-
-  voxel->external()->setForce(x, y, z);
-}
-
-Napi::Value position(const Napi::CallbackInfo& info) {
-  CVX_Voxel* voxel = reinterpret_cast<CVX_Voxel*>(info.Data());
-  auto position = voxel->position();
-  Napi::Array positionArray = Napi::Array::New(info.Env(), 3);
-  positionArray[(uint32_t) 0] = Napi::Number::New(info.Env(), position.x);
-  positionArray[(uint32_t) 1] = Napi::Number::New(info.Env(), position.y);
-  positionArray[(uint32_t) 2] = Napi::Number::New(info.Env(), position.z);
-  return positionArray;
-}
-
 void Voxelyze::getVoxel(const Napi::CallbackInfo& info) {
   Napi::Env env = info.Env();
   float x = info[0].As<Napi::Number>().FloatValue();
@@ -162,7 +219,8 @@ void Voxelyze::getVoxel(const Napi::CallbackInfo& info) {
 
   voxelCallbackObject.DefineProperties({
     Napi::PropertyDescriptor::Function(env, voxelCallbackObject, "setForce", setForce, napi_property_attributes::napi_default, reinterpret_cast<void*>(voxel)),
-    Napi::PropertyDescriptor::Function(env, voxelCallbackObject, "position", position, napi_property_attributes::napi_default, reinterpret_cast<void*>(voxel))
+    Napi::PropertyDescriptor::Function(env, voxelCallbackObject, "position", position, napi_property_attributes::napi_default, reinterpret_cast<void*>(voxel)),
+    Napi::PropertyDescriptor::Function(env, voxelCallbackObject, "setFixedAll", setFixedAll, napi_property_attributes::napi_default, reinterpret_cast<void*>(voxel))
   });
 
   cb.Call(env.Global(), { voxelCallbackObject });
