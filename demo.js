b0a16efd4f47efa565cea2fd24b32228dde72255
Filip Saulean <fsaulean@uvm.edu>
2020-02-03 17:43:42 -0500

    Fix comments space

--- demo.js
+++ demo.js
@@ -11,13 +11,13 @@ const voxel1 = vx.setVoxel(pMaterial, 0, 0, 0);
 const voxel2 = vx.setVoxel(pMaterial, 1, 0, 0);
 const voxel3 = vx.setVoxel(pMaterial, 2, 0, 0);
 
-// fixes the first voxel so that it doesn't move
+//fixes the first voxel so that it doesn't move
 voxel1.setFixedAll();
 
-// sets a force in the negative z direction on the third voxel
+//sets a force in the negative z direction on the third voxel
 voxel3.setForce(0, 0, -1);
 
-// simulates 100 time steps
+//simulates 100 time steps
 for(let i = 0; i < 100; i++) {
   vx.doTimeStep();
 }
